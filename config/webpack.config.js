65b42aef9053cece8e1d25b1cefa05e86ed4864e
DESKTOP-9PLJV5Q\Anderson <andersonjoseph@mailfence.com>
2022-05-29 01:28:29 -0400

    add glsl loaders

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -362,7 +362,14 @@ module.exports = function (webpackEnv) {
                   maxSize: imageInlineSizeLimit,
                 },
               },
-            },
+	    },
+	    {
+	      test: /\.(glsl|frag|vert)$/,
+	      use: [
+		require.resolve('raw-loader'),
+		require.resolve('glslify-loader'),
+	      ]
+	    },
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
